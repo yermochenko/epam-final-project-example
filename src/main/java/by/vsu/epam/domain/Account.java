00c9c0e234f97ad94b0118375ce3ae7ce5ce0be3
Sergey Yermochenko <yermochenko@gmail.com>
2021-05-04 10:36:56 +0300

    Добавлены классы-сущности

---
+++ src/main/java/by/vsu/epam/domain/Account.java
@@ -0,0 +1,33 @@
+package by.vsu.epam.domain;
+
+import java.util.List;
+
+public class Account extends Entity {
+    private String client;
+    private Long balance;
+    private List<Transfer> history;
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public List<Transfer> getHistory() {
+        return history;
+    }
+
+    public void setHistory(List<Transfer> history) {
+        this.history = history;
+    }
+}
---
+++ src/main/java/by/vsu/epam/domain/Role.java
@@ -0,0 +1,21 @@
+package by.vsu.epam.domain;
+
+public enum Role {
+    CASHIER("role.cashier"),
+    MANAGER("role.manager"),
+    ADMINISTRATOR("role.admin");
+
+    private String name;
+
+    private Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return Long.valueOf(ordinal());
+    }
+
+    public String getName() {
+        return name;
+    }
+}
